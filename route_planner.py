686cabb4580d49dbd2a51835bad7d4f3e5c14678
golden-gecko <golden-gecko@holisz.pl>
2026-05-24 19:04:01 +0200

    Load tree from disc

--- route_planner.py
+++ route_planner.py
@@ -1,3 +1,4 @@
+from qgis.core import QgsProject, QgsRasterLayer
 from qgis.gui import QgsMapToolPan
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QIcon
@@ -10,12 +11,15 @@ from .options import Options
 from .route_planner_dockwidget import RoutePlannerDockWidget
 from .segment import Segment
 from .track import Track
+from .tree import Tree
 
 from .resources import *
 
 
 class RoutePlanner:
     def __init__(self, iface):
+        print('RoutePlanner.__init__()')
+
         Iface.set(iface)
 
         self.iface = iface
@@ -39,82 +43,101 @@ class RoutePlanner:
         self.mapToolPointMove = PointMove(self.iface, self.iface.mapCanvas())
         self.mapToolPointDelete = PointDelete(self.iface, self.iface.mapCanvas())
 
-    def add_action(self, icon_path, text, callback, parent = None):
+    def add_action(self, icon_path: str, text: str, callback, parent = None):
+        print('RoutePlanner.add_action()')
+
         action = QAction(QIcon(icon_path), text, parent)
         action.triggered.connect(callback)
         action.setEnabled(True)
 
         self.toolbar.addAction(action)
         self.iface.addPluginToMenu(self.menu, action)
-        self.actions.append(action)
+
+        return action
 
     def initGui(self):
-        self.add_action(':/plugins/route_planner/icon.png', text='Show', callback=self.run, parent=self.iface.mainWindow())
+        print('RoutePlanner.initGui()')
+
+        action = self.add_action(':/plugins/route_planner/icon.png', text='Show', callback=self.run, parent=self.iface.mainWindow())
+
+        if action is not None:
+            self.actions.append(action)
 
     def onClosePlugin(self):
+        print('RoutePlanner.onClosePlugin()')
+
+        return
         self.dockwidget.closingPlugin.disconnect(self.onClosePlugin)
         self.dockwidget = None
 
     def unload(self):
-        self.iface.removeDockWidget(self.dockwidget)
-        self.dockwidget.deleteLater()
+        print('RoutePlanner.unload()')
 
         for action in self.actions:
             self.iface.removePluginMenu('&RoutePlanner', action)
             self.iface.removeToolBarIcon(action)
 
+        if self.dockwidget is not None:
+            self.iface.removeDockWidget(self.dockwidget)
+            self.dockwidget.deleteLater()
+
     def run(self):
-        self.dockwidget = RoutePlannerDockWidget()
-        self.dockwidget.closingPlugin.connect(self.onClosePlugin)
+        print('RoutePlanner.run()')
 
-        # main modes
-        self.dockwidget.buttonSelect.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPan))
-        self.dockwidget.buttonEdit.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolEdit))
+        if self.dockwidget is None:
+            # create widget
+            self.dockwidget = RoutePlannerDockWidget()
+            self.dockwidget.closingPlugin.connect(self.onClosePlugin)
 
-        # setup file buttons
-        self.dockwidget.buttonFileNew.clicked.connect(lambda: Segment.create(Track.create(File.new())))
-        self.dockwidget.buttonFileOpen.clicked.connect(lambda: File.open())
-        self.dockwidget.buttonFileReload.clicked.connect(lambda: File.reload(File.get_active(self.iface)))
-        self.dockwidget.buttonFileSave.clicked.connect(lambda: File.save(File.get_active(self.iface)))
-        self.dockwidget.buttonFileClose.clicked.connect(lambda: File.close(File.get_active(self.iface)))
+            # main modes
+            self.dockwidget.buttonLoad.clicked.connect(lambda: Tree.create_tree_structure())
+            self.dockwidget.buttonSelect.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPan))
+            self.dockwidget.buttonEdit.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolEdit))
 
-        # setup waypoint buttons
-        self.dockwidget.buttonWaypointCreate.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolWaypointCreate))
-        self.dockwidget.buttonWaypointMove.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolWaypointMove))
-        self.dockwidget.buttonWaypointDelete.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolWaypointDelete))
+            # setup file buttons
+            self.dockwidget.buttonFileNew.clicked.connect(lambda: Segment.create(Track.create(File.new())))
+            self.dockwidget.buttonFileOpen.clicked.connect(lambda: File.open())
+            self.dockwidget.buttonFileReload.clicked.connect(lambda: File.reload(File.get_active(self.iface)))
+            self.dockwidget.buttonFileSave.clicked.connect(lambda: File.save(File.get_active(self.iface)))
+            self.dockwidget.buttonFileClose.clicked.connect(lambda: File.close(File.get_active(self.iface)))
 
-        # setup track buttons
-        self.dockwidget.buttonTrackCreate.clicked.connect(lambda: Segment.create(Track.create(File.get_active(self.iface))))
-        self.dockwidget.buttonTrackRefresh.clicked.connect(lambda: Track.refresh(Track.get_active(self.iface)))
-        self.dockwidget.buttonTrackReverse.clicked.connect(lambda: Track.reverse(Track.get_active(self.iface)))
-        self.dockwidget.buttonTrackOptimize.clicked.connect(lambda: Track.optimize(Track.get_active(self.iface)))
-        self.dockwidget.buttonTrackDelete.clicked.connect(lambda: Track.delete(Track.get_active(self.iface)))
+            # setup waypoint buttons
+            self.dockwidget.buttonWaypointCreate.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolWaypointCreate))
+            self.dockwidget.buttonWaypointMove.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolWaypointMove))
+            self.dockwidget.buttonWaypointDelete.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolWaypointDelete))
 
-        # setup segment buttons
-        self.dockwidget.buttonSegmentCreate.clicked.connect(lambda: Segment.create(Track.get_active(self.iface)))
-        self.dockwidget.buttonSegmentRefresh.clicked.connect(lambda: Segment.refresh(Segment.get_active(self.iface)))
-        self.dockwidget.buttonSegmentReverse.clicked.connect(lambda: Segment.reverse(Segment.get_active(self.iface)))
-        self.dockwidget.buttonSegmentDelete.clicked.connect(lambda: Segment.delete(Segment.get_active(self.iface)))
+            # setup track buttons
+            self.dockwidget.buttonTrackCreate.clicked.connect(lambda: Segment.create(Track.create(File.get_active(self.iface))))
+            self.dockwidget.buttonTrackRefresh.clicked.connect(lambda: Track.refresh(Track.get_active(self.iface)))
+            self.dockwidget.buttonTrackReverse.clicked.connect(lambda: Track.reverse(Track.get_active(self.iface)))
+            self.dockwidget.buttonTrackOptimize.clicked.connect(lambda: Track.optimize(Track.get_active(self.iface)))
+            self.dockwidget.buttonTrackDelete.clicked.connect(lambda: Track.delete(Track.get_active(self.iface)))
 
-        # setup point buttons
-        self.dockwidget.buttonPointCreateStart.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointCreateStart))
-        self.dockwidget.buttonPointCreateMiddle.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointCreateMiddle))
-        self.dockwidget.buttonPointCreateEnd.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointCreateEnd))
-        self.dockwidget.buttonPointMove.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointMove))
-        self.dockwidget.buttonPointDelete.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointDelete))
+            # setup segment buttons
+            self.dockwidget.buttonSegmentCreate.clicked.connect(lambda: Segment.create(Track.get_active(self.iface)))
+            self.dockwidget.buttonSegmentRefresh.clicked.connect(lambda: Segment.refresh(Segment.get_active(self.iface)))
+            self.dockwidget.buttonSegmentReverse.clicked.connect(lambda: Segment.reverse(Segment.get_active(self.iface)))
+            self.dockwidget.buttonSegmentDelete.clicked.connect(lambda: Segment.delete(Segment.get_active(self.iface)))
 
-        # setup routing options
-        self.dockwidget.optionRouting.stateChanged.connect(lambda: Options.set_routing(self.dockwidget.optionRouting.isChecked()))
-        self.dockwidget.optionRoutingProvider.currentTextChanged.connect(lambda: Options.set_routing_provider(self.dockwidget.optionRoutingProvider.currentText()))
-        self.dockwidget.optionRoutingMode.currentTextChanged.connect(lambda: Options.set_routing_mode(self.dockwidget.optionRoutingMode.currentText()))
+            # setup point buttons
+            self.dockwidget.buttonPointCreateStart.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointCreateStart))
+            self.dockwidget.buttonPointCreateMiddle.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointCreateMiddle))
+            self.dockwidget.buttonPointCreateEnd.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointCreateEnd))
+            self.dockwidget.buttonPointMove.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointMove))
+            self.dockwidget.buttonPointDelete.clicked.connect(lambda: self.iface.mapCanvas().setMapTool(self.mapToolPointDelete))
 
-        # setup control point options
-        self.dockwidget.spinBoxPointsPerSegment.setValue(Options.control_point_per_segment)
-        self.dockwidget.spinBoxPointsPerSegment.valueChanged.connect(lambda: Options.set_control_point_per_segment(self.dockwidget.spinBoxPointsPerSegment.value()))
-        self.dockwidget.spinBoxMinPointDistance.setValue(Options.control_point_min_distance)
-        self.dockwidget.spinBoxMinPointDistance.valueChanged.connect(lambda: Options.set_control_point_min_distance(self.dockwidget.spinBoxMinPointDistance.value()))
-        self.dockwidget.spinBoxMinAngle.setValue(Options.control_point_min_angle)
-        self.dockwidget.spinBoxMinAngle.valueChanged.connect(lambda: Options.set_control_point_min_angle(self.dockwidget.spinBoxMinAngle.value()))
+            # setup routing options
+            self.dockwidget.optionRouting.stateChanged.connect(lambda: Options.set_routing(self.dockwidget.optionRouting.isChecked()))
+            self.dockwidget.optionRoutingProvider.currentTextChanged.connect(lambda: Options.set_routing_provider(self.dockwidget.optionRoutingProvider.currentText()))
+            self.dockwidget.optionRoutingMode.currentTextChanged.connect(lambda: Options.set_routing_mode(self.dockwidget.optionRoutingMode.currentText()))
+
+            # setup control point options
+            self.dockwidget.spinBoxPointsPerSegment.setValue(Options.control_point_per_segment)
+            self.dockwidget.spinBoxPointsPerSegment.valueChanged.connect(lambda: Options.set_control_point_per_segment(self.dockwidget.spinBoxPointsPerSegment.value()))
+            self.dockwidget.spinBoxMinPointDistance.setValue(Options.control_point_min_distance)
+            self.dockwidget.spinBoxMinPointDistance.valueChanged.connect(lambda: Options.set_control_point_min_distance(self.dockwidget.spinBoxMinPointDistance.value()))
+            self.dockwidget.spinBoxMinAngle.setValue(Options.control_point_min_angle)
+            self.dockwidget.spinBoxMinAngle.valueChanged.connect(lambda: Options.set_control_point_min_angle(self.dockwidget.spinBoxMinAngle.value()))
 
         # show widget
         self.iface.addDockWidget(Qt.LeftDockWidgetArea, self.dockwidget)
