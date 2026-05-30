82559c525af808590dc0fd2fb620696460c262cb
golden-gecko <golden-gecko@holisz.pl>
2026-05-30 11:11:45 +0200

    Add Street View (#5)

--- route_planner_dockwidget.py
+++ route_planner_dockwidget.py
@@ -3,6 +3,11 @@ import os
 from qgis.PyQt import QtWidgets, uic
 from qgis.PyQt.QtCore import pyqtSignal
 
+try:
+    from qgis.PyQt.QtWebEngineWidgets import QWebEngineView
+except ImportError:
+    QWebEngineView = None
+
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(str(__file__)), 'route_planner_dockwidget_base.ui'))
 
@@ -14,6 +19,24 @@ class RoutePlannerDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         super(RoutePlannerDockWidget, self).__init__(parent)
 
         self.setupUi(self)
+        self.streetViewBrowser = None
+
+        if QWebEngineView is None:
+            self.labelStreetView.setText('QtWebEngine is not available')
+            return
+
+        self.streetViewBrowser = QWebEngineView(self)
+        self.streetViewBrowser.setMinimumHeight(220)
+
+        index = self.gridLayout.indexOf(self.labelStreetView)
+
+        if index < 0:
+            return
+
+        row, column, row_span, column_span = self.gridLayout.getItemPosition(index)
+        self.gridLayout.removeWidget(self.labelStreetView)
+        self.labelStreetView.deleteLater()
+        self.gridLayout.addWidget(self.streetViewBrowser, row, column, row_span, column_span)
 
     def closeEvent(self, event):
         self.closingPlugin.emit()
